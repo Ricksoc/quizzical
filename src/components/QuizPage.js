6cce7f93341aa879dad35fa935963efaa46fa67b
ricky <ricky.chaggar@gmail.com>
2022-05-24 16:11:02 +0100

    Dynamically Generate Quiz Section
    Questions ans answers dynamically rendered as html content from
    API call.
    
    Text is cleaned up using .replace to fix "" and ' issues.
    
    Durtenfield shuffle algorithm used to randomise the location of the
    cirrect answer within the provided options.

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 export default function QuizPage(props) {
-  const questions = (
-    <div className="quiz__section">
-      <h2 className="quiz__question">What is the answer to this quesstion?</h2>
-      <button className="quiz__answer">One</button>
-      <button className="quiz__answer">Two</button>
-      <button className="quiz__answer">Three</button>
-      <button className="quiz__answer">Four</button>
-    </div>
-  );
+  console.log(props.questions);
+
+  // Durstenfield shuffle algorithm to randomise array of answers
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
+  const quiz = props.questions.map((question) => {
+    // Get array of answers and shuffle it
+    const answers = [];
+    answers.push(
+      question.correct_answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'")
+    );
+    question.incorrect_answers.forEach((answer) => {
+      answers.push(answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'"));
+    });
+    shuffleArray(answers);
+
+    return (
+      <div className="quiz__section" key={nanoid()}>
+        <h2 className="quiz__question">
+          {question.question.replace(/&quot;/g, '"').replace(/&#039;/g, "'")}
+        </h2>
+        {answers.map((answer) => {
+          return (
+            <button className="quiz__answer">
+              {answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'")}
+            </button>
+          );
+        })}
+      </div>
+    );
+  });
 
   return (
     <div className="quiz__container">
       <div className="blob blob__top__quiz"></div>
       <div className="blob blob__bottom__quiz"></div>
-      {questions}
+      {quiz}
       <button className="quiz__check" onClick={props.handleClick}>
         Check Answers
       </button>
