21539d2dee67321695ac1e08586db0fbe2d033e6
ricky <ricky.chaggar@gmail.com>
2022-05-24 16:09:04 +0100

    Make API Request
    Use asycn await to make API request to Open Trivia and save returned
    json in state which is then passed to QuizPage as props.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import StartPage from "./StartPage";
 import QuizPage from "./QuizPage";
 
 export default function App() {
   const [startQuiz, setStartQuiz] = useState(false);
+  const [questions, setQuestions] = useState([]);
 
-  function beginQuiz() {
+  async function beginQuiz() {
+    const response = await fetch("https://opentdb.com/api.php?amount=5");
+    const data = await response.json();
+    setQuestions(data.results);
     setStartQuiz((prevState) => !prevState);
-    console.log(startQuiz);
   }
 
   return (
     <main>
       {/* <StartPage handleClick={beginQuiz} /> */}
       {startQuiz ? (
-        <QuizPage handleClick={beginQuiz} />
+        <QuizPage handleClick={beginQuiz} questions={questions} />
       ) : (
         <StartPage handleClick={beginQuiz} />
       )}
